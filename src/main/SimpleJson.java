0449a6c7e02bbfbb641dea3ca007ea107f4c3e17
hpmss <arestvn@gmail.com>
2020-12-29 14:27:26 +0700

    First commit

---
+++ src/main/SimpleJson.java
@@ -0,0 +1,433 @@
+/**
+* 
+* This is a very simple JSON parser, that can parse raw JSON string and convert them to Java Map.
+* There is no error handling and everything is packed inside a single java file.
+* Prefer to main function for examples usage.
+* 
+* @author SASANQUA
+* @since  29-12-2020
+ */
+
+package main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class SimpleJson {
+	
+	private static final char OBJECT_BEGIN_CHAR = '{';
+	private static final char OBJECT_END_CHAR = '}';
+	private static final char ARRAY_BEGIN_CHAR = '[';
+	private static final char ARRAY_END_CHAR = ']';
+	private static final char STRING_CHAR = '"';
+	private static final char KEY_SEPERATOR_CHAR = ':';
+	private static final char VALUE_SEPERATOR_CHAR = ',';
+	private static final char NUMBER_SEPERATOR_CHAR = '.';
+	
+	private static enum TType {
+		OBJECT_BEGIN,
+		OBJECT_END,
+		ARRAY_BEGIN,
+		ARRAY_END,
+		STRING,
+		KEY_SEPERATOR,
+		VALUE_SEPERATOR,
+		NUMBER,
+		NULL,
+		TRUE,
+		FALSE
+		
+	}
+	
+	private static class Token {
+		final TType type;
+		final String lexeme;
+		final Object literal;
+		final int line;
+		
+		public Token(TType type, String lexeme, Object literal, int line) {
+			this.type = type;
+			this.lexeme = lexeme;
+			this.literal = literal;
+			this.line = line;
+		}
+		
+		public String toString() {
+			return "[line" + line +  "]" + type + " " + lexeme + " " + literal;
+		}
+		
+	}
+	
+	private static class Scanner {
+		private List<Token> tokens;
+		private int start = 0;
+		private int current = 0;
+		private int line = 1;
+		private String source;
+		private static final Map<String, TType> keywords;
+		
+		static {
+			keywords = new HashMap<String, TType>();
+			keywords.put("null", TType.NULL);
+			keywords.put("true", TType.TRUE);
+			keywords.put("false", TType.FALSE);
+		}
+		
+		public static Scanner getScanner(String source) {
+			return new Scanner(source);
+		}
+		
+		private Scanner() {}
+		
+		private Scanner(String source) {
+			this.source = source;
+			this.tokens = new ArrayList<Token>();
+		}
+		
+		public List<Token> scanTokens() {
+			while(!isEOF()) {
+				start = current;
+				scanToken();
+			}
+			return tokens;
+		}
+		
+		private void scanToken() {
+			char c = advance();
+			switch(c) {
+			case ' ':
+			case '\r':
+			case '\t':
+				break;
+			case OBJECT_BEGIN_CHAR:
+				object();
+				break;
+			case ARRAY_BEGIN_CHAR:
+				array();
+				break;
+			case STRING_CHAR:
+				string();
+				break;
+			case KEY_SEPERATOR_CHAR:
+				addToken(TType.KEY_SEPERATOR);
+				break;
+			case VALUE_SEPERATOR_CHAR:
+				addToken(TType.VALUE_SEPERATOR);
+				break;
+			case '\n':
+				line++;
+				break;
+			default:
+				if(isDigit(c)) {
+					number();
+				}else if(isAlpha(c)) {
+					identifier();
+				}else {
+					throw new RuntimeException("Unexpected token.");
+				}
+				break;
+			}
+		}
+		
+		private void object() {
+			addToken(TType.OBJECT_BEGIN);
+			while(!isEOF() && peek(0) != OBJECT_END_CHAR) {
+				start = current;
+				scanToken();
+			}
+			if(!match(OBJECT_END_CHAR)) throw new RuntimeException("Unterminated object.");
+			start = current - 1;
+			addToken(TType.OBJECT_END);
+		}
+		
+		private void array() {
+			addToken(TType.ARRAY_BEGIN);
+			while(!isEOF() && peek(0) != ARRAY_END_CHAR) {
+				start = current;
+				scanToken();
+			}
+			if(!match(ARRAY_END_CHAR)) throw new RuntimeException("Unterminated array.");
+			start = current - 1;
+			addToken(TType.ARRAY_END);
+		}
+		
+		private void string() {
+			while(!isEOF() && peek(0) != STRING_CHAR) {
+				if(peek(0) == '\n') line++;
+				advance();
+			}
+			if(isEOF()) throw new RuntimeException("Unterminated string.");
+			advance();
+			
+			String value = source.substring(start + 1, current - 1);
+			addToken(TType.STRING, value);
+		}
+		
+		private void number() {
+			while(isDigit(peek(0))) advance();
+			
+			if(peek(0) == NUMBER_SEPERATOR_CHAR && isDigit(peek(1))) {
+				advance();
+				while(isDigit(peek(0))) advance();
+			}
+			addToken(TType.NUMBER, Double.parseDouble(source.substring(start, current)));
+		}
+		
+		private void identifier() {
+			while(isAlphaNumeric(peek(0))) advance();
+			
+			String text = source.substring(start, current);
+			TType type = keywords.get(text);
+			if(text == null) type = TType.STRING;
+			
+			if(type == TType.STRING) addToken(type, text);
+			else addToken(type);
+		}
+		
+		private char advance() {
+			return source.charAt(current++);
+		}
+		
+		private char peek(int offset) {
+			if(current + offset >= source.length()) return '\0';
+			return source.charAt(current + offset);
+		}
+		
+		private boolean match(char expected) {
+			if(isEOF()) return false;
+			if(peek(0) != expected) return false;
+			advance();
+			return true;
+		}
+		
+		
+		private boolean isDigit(char c) {
+			return c >= '0' && c <= '9';
+		}
+		
+		private boolean isAlpha(char c) {
+			return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+		}
+		
+		private boolean isAlphaNumeric(char c) {
+			return isDigit(c) || isAlpha(c);
+		}
+		
+		private boolean isEOF() {
+			return current >= source.length();
+		}
+		
+		
+		private void addToken(TType type) {
+			addToken(type, null);
+		}
+		
+		private void addToken(TType type, Object literal) {
+			String text = source.substring(start, current);
+			tokens.add(new Token(type, text, literal, line));
+		}
+	
+	}
+	
+	private static class Parser {
+		
+		private Map<String, Object> parent;
+		private Object child;
+		private Stack<Object> stack;
+		private List<Token> tokens;
+		private Token prevToken;
+		private Token currToken;
+		private int i;
+		
+		private Parser() {}
+		
+		private Parser(List<Token> tokens) {
+			this.parent = new HashMap<String, Object>();
+			this.stack = new Stack<Object>();
+			this.tokens = tokens;
+		}
+		
+		
+		public static Parser getParser(List<Token> tokens) {
+			return new Parser(tokens);
+		}
+		
+		public Map<String, Object> parse() {
+			if(tokens.size() == 0) return parent;
+			
+			prevToken = tokens.get(0);
+			
+			while(!isEnd()) {
+				parseToken();
+			}
+			
+			return parent;
+		}
+		
+		private void parseToken() {
+			currToken = tokens.get(i++);
+			child = stack.isEmpty() ? parent : stack.peek();
+			
+			switch(currToken.type) {
+			case OBJECT_BEGIN:
+				parseObject();
+				break;
+			case STRING:
+				parseString();
+				break;
+			case NUMBER:
+				parseNumber();
+				break;
+			case VALUE_SEPERATOR:
+				break;
+			case ARRAY_BEGIN:
+				parseArray();
+				break;
+			case KEY_SEPERATOR:
+				break;
+			case OBJECT_END:
+				if(!stack.isEmpty()) {
+					stack.pop();
+				}
+				break;
+			case ARRAY_END:
+				if(!stack.isEmpty()) {
+					stack.pop();
+				}
+				break;
+			case NULL:
+				parseNull();
+				break;
+			case TRUE:
+				parseTrue();
+				break;
+			case FALSE:
+				parseFalse();
+				break;
+			default:
+				throw new RuntimeException("Unexpected token.");
+			}
+			
+			if(currToken.type != TType.KEY_SEPERATOR) prevToken = currToken;
+		}
+		
+		private void parseObject() {
+			putValue(new LinkedHashMap<String, Object>());
+			
+			if(prevToken.type == TType.STRING) {
+				if(child instanceof Map) {
+					stack.push(((Map<String, Object>) child).get(prevToken.literal.toString()));
+				}
+			}
+			
+			if(child instanceof List) {
+				stack.push(((List<Object>) child).get(((List<Object>) child).size() - 1));
+			}
+			
+			prevToken = currToken;
+			
+			while(!isEnd() && currToken.type != TType.OBJECT_END) {
+				parseToken();
+			}
+			
+		}
+		
+		private void parseArray() {
+			putValue(new ArrayList<Object>());
+			
+			if(prevToken.type == TType.STRING) {
+				if(child instanceof Map) {
+					stack.push(((Map<String, Object>) child).get(prevToken.literal.toString()));
+				}
+			}
+			
+			if(child instanceof List) {
+				stack.push(((List<Object>) child).get(((List<Object>) child).size() - 1));
+			}
+			
+			prevToken = currToken;
+			
+			while(!isEnd() && currToken.type != TType.ARRAY_END) {
+				parseToken();
+			}
+		}
+		
+		private void parseString() {
+			putValue(currToken.literal.toString());
+		}
+		
+		private void parseNumber() {
+			putValue(currToken.literal);
+		}
+		
+		private void parseNull() {
+			putValue(null);
+		}
+		
+		private void parseTrue() {
+			putValue(true);
+			
+		}
+		
+		private void parseFalse() {
+			putValue(false);
+			
+		}
+		
+		private void putValue(Object v) {
+			
+			if(prevToken.type == TType.STRING) {
+				if(child instanceof Map) {
+					((Map<String, Object>) child).put(prevToken.literal.toString(), v);
+				}
+			}
+			
+			if(child instanceof List) {
+				((List<Object>) child).add(v);
+			}
+			
+		}
+		
+		private boolean isEnd() {
+
+			return i >= tokens.size();
+		}
+	}
+	
+	public static Map<String, Object> parse(String json) {
+		return Parser.getParser(Scanner.getScanner(json).scanTokens()).parse();	
+	}
+	
+	public static void main(String[] args) {
+		
+		String[] examples = new String[3];
+		examples[0] = "[   \"One   \" , 123  , \"   Two  \"   ,   \"    Three   \", 12131.131312, 5113.111, [[1, 3, 5,2,3,4]   ], {\"a\": 123, \"b\": \"c\"}]";
+		examples[1] = "{\r\n" + 
+				"    \"glossary\": {\r\n" + 
+				"        \"title\": \"example glossary\",\r\n" + 
+				"		\"GlossDiv\": {\r\n" + 
+				"            \"title\": \"S\",\r\n" + 
+				"			\"GlossList\": {\r\n" + 
+				"                \"GlossEntry\": {\r\n" + 
+				"                    \"ID\": \"SGML\",\r\n" + 
+				"					\"SortAs\": \"SGML\",\r\n" + 
+				"					\"GlossTerm\": \"Standard Generalized Markup Language\",\r\n" + 
+				"					\"Acronym\": \"SGML\",\r\n" + 
+				"					\"Abbrev\": \"ISO 8879:1986\",\r\n" + 
+				"					\"GlossSee\": \"markup\"\r\n" + 
+				"                }\r\n" + 
+				"            }\r\n" + 
+				"        }\r\n" + 
+				"    }\r\n" + 
+				"}";
+		examples[2] = "{\"name\":\"John\", \"age\": true, \"city\":\"New York\", \"a\": {\"b\": {\"c\": 123, \"e\": [1,2,3,4,5, [5,6,7,8,9, {\"vip\": \"pro\"}, \"hello world\"]]}}}, \"d\": 10";
+		System.out.println(parse(examples[0]));
+		System.out.println(parse(examples[1]));
+		System.out.println(parse(examples[2]));
+	}
+	
+}
